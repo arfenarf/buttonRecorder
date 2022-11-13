4154f5511471676ae0a3a1a2fe64ebe5eb4a7b8f
Kate Weber <kate@kateweber.com>
2022-11-13 10:20:42 -0500

    Migrate to docker-compose

--- start_button_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker run --name api-button-recorder \
-           -d \
-           -p 8000:8000 \
-           --env-file envs.env \
-           --add-host=database:192.168.1.32 \
-           api/button-recorder:0.0
