1287e7b1880e1c054939820c3b5c605b04c6194b
Katherine Weber <kate@Katherines-MacBook-Pro.local>
2022-11-12 20:04:09 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+import psycopg2
+from datetime import datetime, timezone
+
+
+app = FastAPI()
+
+def get_db_connection():
+    connection = psycopg2.connect(host='tofino.local',
+                                  dbname='kgw_data',
+                                  user='postgres',
+                                  password='green-egg',
+                                  port=25432)
+    return connection
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/button")
+async def save_button_time():
+    with get_db_connection() as connection:
+        with connection.cursor() as curs:
+            utc_dt = datetime.now(timezone.utc)
+            curs.execute("""
+                INSERT INTO button_presses (date_time_pressed)
+                VALUES (%s)
+            """, (utc_dt, ))
+        curs.close()
+
+    return {"messages": f"posted {utc_dt}"}
